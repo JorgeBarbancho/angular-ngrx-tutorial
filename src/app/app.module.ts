2a10d89f597a3ffbada8f9c1429e046a3a9cb286
JorgeBarbancho <jbarbancho@gmail.com>
2021-12-21 09:03:47 +0100

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { collectionReducer } from './state/collection.reducer';
 import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookCollectionComponent } from './book-collection/book-collection.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
+    HttpClientModule,
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, BookListComponent, BookCollectionComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
